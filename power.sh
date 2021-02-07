76ece1b85726e7cb744ac4f7d44a636993464412
rlmajor <61337954+rlmajor@users.noreply.github.com>
2021-02-06 23:33:13 -0500

    Update power.sh

--- power.sh
+++ power.sh
@@ -4,7 +4,10 @@ echo "How many watts does your device use?:"
 read devicewatts
 echo "How many hours are you going to run it?"
 read hours
+
+#Change the below to whatever your power provides you per kWh, you can find it on your bill.
 centsperhour=".17"
+
 watthours=`expr $devicewatts \* $hours`
 kwhours=$(($watthours / 1000))
 total=`echo $kwhours \* $centsperhour|bc`;
