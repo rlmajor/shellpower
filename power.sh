5caa67eadad0c74b28aeb99ff3c360cf2c9adab3
rlmajor <61337954+rlmajor@users.noreply.github.com>
2021-02-06 23:28:38 -0500

    Create power.sh

---
+++ power.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+echo "How many watts does your device use?:"
+read devicewatts
+echo "How many hours are you going to run it?"
+read hours
+centsperhour=".17"
+watthours=`expr $devicewatts \* $hours`
+kwhours=$(($watthours / 1000))
+total=`echo $kwhours \* $centsperhour|bc`;
+echo "It costs \$$total to run your $devicewatts device for $hours hours."
+
